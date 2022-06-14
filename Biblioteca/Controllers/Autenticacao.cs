bfd0a8b6b7621d32c33bcc0f0228d14837448c0d
Jhones Yure <jymbomfim@hotmail.com>
2022-06-14 18:37:03 -0300

    autenticação modificada

--- Biblioteca/Controllers/Autenticacao.cs
+++ Biblioteca/Controllers/Autenticacao.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System;
+using Biblioteca.Models;
+using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,9 +12,57 @@ namespace Biblioteca.Controllers
     {
         public static void CheckLogin(Controller controller)
         {   
-            if(string.IsNullOrEmpty(controller.HttpContext.Session.GetString("user")))
+            if(string.IsNullOrEmpty(controller.HttpContext.Session.GetString("login")))
             {
                 controller.Request.HttpContext.Response.Redirect("/Home/Login");
+            }
+        }
+        public static bool VerificaLoginSenha(string login, string senha, Controller controller)
+        {
+            using(BibliotecaContext bc = new BibliotecaContext())
+            {
+                
+
+
+                IQueryable<Usuario> UsuarioEncontrado = bc.Usuarios.Where(u => u.Login == login && u.Senha== senha);
+                List<Usuario> ListaUsuarioEncontrado = UsuarioEncontrado.ToList();
+
+                if(ListaUsuarioEncontrado.Count==0)
+                {
+                    return false;
+                } else 
+                {
+                    controller.HttpContext.Session.SetString("login", ListaUsuarioEncontrado[0].Login);
+                    controller.HttpContext.Session.SetString("nome", ListaUsuarioEncontrado[0].Nome);
+                    controller.HttpContext.Session.SetInt32("tipo", ListaUsuarioEncontrado[0].Tipo);
+
+                    return true;
+                }
+            }
+        }
+        public static void verificarAdmin(BibliotecaContext bc)
+        {
+          
+               IQueryable<Usuario> UsuarioEncontrado = bc.Usuarios.Where(u => u.Login == "admin");
+
+               if (UsuarioEncontrado.ToList().Count==0)
+               {
+                   Usuario admin = new Usuario();
+                   admin.Nome = "Administrador";
+                   admin.Login = "admin";
+                   //admin.Senha =
+                   admin.Tipo = Usuario.ADMIN;
+
+                   bc.Usuarios.Add(admin);
+                   bc.SaveChanges();
+               }
+           
+        }
+        public static void verificarUsuarioAdmin(Controller controller)
+        {
+            if(controller.HttpContext.Session.GetInt32("tipo")== Usuario.ADMIN)
+            {
+                controller.Request.HttpContext.Response.Redirect("/Usuarios/NeedAdmin");
             }
         }
     }
