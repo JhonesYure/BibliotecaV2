755009e708368e229f11632cafbf484956b93e22
Jhones Yure <jymbomfim@hotmail.com>
2022-06-14 17:02:20 -0300

    Criação das Models Usuario

---
+++ Biblioteca/Models/UsuarioService.cs
@@ -0,0 +1,56 @@
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Biblioteca.Models
+{
+    public class UsuarioService
+    {
+        //LISTAR
+        public List<Usuario> Listar()
+        {
+            using(BibliotecaContext bc = new BibliotecaContext())
+            {
+                return bc.Usuarios.ToList();
+            }
+        }
+        public Usuario Listar(int id)
+        {
+            using(BibliotecaContext bc = new BibliotecaContext())
+            {
+                return bc.Usuarios.Find(id);
+            }
+        }
+        //INCLUIR
+        public void Inserir(Usuario novoUsuario)
+        {
+            using(BibliotecaContext bc = new BibliotecaContext())
+            {
+               bc.Usuarios.Add(novoUsuario);
+               bc.SaveChanges();
+            }
+        }
+        //EDITAR
+            public void EditarUsuario(Usuario editaUsuario)
+            {
+                using(BibliotecaContext bc = new BibliotecaContext())
+                {
+                    Usuario u = bc.Usuarios.Find(editaUsuario.ID);
+                    u.Nome = editaUsuario.Nome;
+                    u.Login = editaUsuario.Login;
+                    u.Senha = editaUsuario.Senha;
+                    u.Tipo = editaUsuario.Tipo;
+
+                    bc.SaveChanges();
+                }
+            }
+            //EXCLUIR
+            public void ExcluirUsuario(int id)
+            {
+                using(BibliotecaContext bc = new BibliotecaContext())
+                {
+                    bc.Usuarios.Remove(bc.Usuarios.Find(id));
+                }
+            }    
+    }
+}
